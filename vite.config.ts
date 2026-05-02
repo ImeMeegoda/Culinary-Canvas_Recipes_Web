6fac3be14f9564f48b6377676c3f78605890c5ad
ime20041126 <imesha.20240948@iit.ac.lk>
2026-05-02 19:47:19 +0530

    fix: update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/imemeegoda.github.io-Culinary-Canvas_Recipes--/',
+  base: '/Culinary-Canvas_Recipes_Web/',
   plugins: [react()],
 })
